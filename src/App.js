db3ca7970303f90edf08b35b95e6ed5e3b8f6a75
uni1023 <53444206+uni1023@users.noreply.github.com>
2020-07-05 13:59:59 +0900

    영화 앱 만들기 시작 06챕터

--- src/App.js
+++ src/App.js
@@ -3,49 +3,23 @@ import React from 'react';
 class App extends React.Component {
   // React.Component 클래스의 기능을 추가한 App 클래스
   // 실제로 React.Component 클래스는 500줄이 넘는 코드로 여러 기능이 구현되어 있음.
-
-  constructor(props){
-  super(props);
-  console.log('hello'); // console 로그를 통해 작동 순서를 체크 할 수 있구나.
-  }
-
   state = {
-    count: 0,
+    isLoading : true,
+    movies: [],
   };
-
-  add = () => {
-    this.setState(current => ({
-      count: current.count + 1,
-    }));
-  };
-
-  minus = () => {
-    this.setState(current => ({
-      count: current.count - 1,
-    }));
-  };
-
   componentDidMount() {
-    console.log('component rendered');
+    // 영화 데이터 로딩!
+    setTimeout( () => {
+      this.setState({ isLoading: false });
+    }, 6000);
+    
   }
 
-  componentDidUpdate() {
-  console.log('I just updated');
-}
 
-  componentWillUnmount() {
-    console.log('Godbye, cruel world');
-  }
-// render 함수는 아래 값을 실제 화면에 나타나게 해줌 렌더링
-  render(){
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1> 
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
   }
 }
 
