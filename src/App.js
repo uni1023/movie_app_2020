d93f55138123b9d1f74099ff8d74875e1fc3df87
uni1023 <53444206+uni1023@users.noreply.github.com>
2020-07-05 13:01:42 +0900

    숫자 증가됨

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ class App extends React.Component {
   };
 
   add = () => {
-    this.setState({ count : this.state.count +1 });
+    this.setState({ count : this.state.count + 1 });
   };
 
   minus = () => {
-    this.setState({ count : this.state.count -1 });
+    this.setState({ count : this.state.count - 1 });
   };
 
-// render 함수는 아래 값을 실제 화면에 나타나게 해줌 '렌더링'
+// render 함수는 아래 값을 실제 화면에 나타나게 해줌 렌더링
   render(){
 
     return (
